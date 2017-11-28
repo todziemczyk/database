552be7523fcbf2dc73309a2e35dd01a0bcf0011b
todziemczyk <t.odziemczyk@gmail.com>
2017-11-28 21:50:39 +0100

    Correct version

--- src/ImportDatabase.groovy
+++ src/ImportDatabase.groovy
@@ -1,3 +1,7 @@
-class ImportDatabase {
-    
-}
+import groovy.sql.Sql;
+
+def sql = Sql.newInstance("jdbc:oracle:thin:@//tgrall-linux:1521/XE",
+        "GROOVY",
+        "GROOVY",
+        "oracle.jdbc.driver.OracleDriver")
+def set = sql.dataSet("NEWS");
